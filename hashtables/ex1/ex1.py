d67356909a7f270c92087bf3ace80be3369e7685
Hunter Raffety <hsraffety@gmail.com>
2019-11-01 11:11:07 -0700

    was in wrong directory and forgot to commit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,20 +1,59 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+
+
+# Given a package with a weight limit limit and a list weights of item weights, implement a function get_indices_of_item_weights that finds two items whose sum of weights equals the weight limit limit. Your function will return an instance of an Answer tuple that has the following form:
+
+# (zero, one)
+
+# where each element represents the item weights of the two packages. The higher valued index should be placed in the zeroth index and the smaller index should be placed in the first index. If such a pair doesn’t exist for the given inputs, your function should return None.
+
+# NOTE: When calling hash_table_retrieve with a key that doesn't exist in the hash table, hash_table_retrieve will return None.
+
+# Your solution should run in linear time.
+
+# Example:
+
+# input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+# output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
+
+
+
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-
+    print(f"weights: {weights} length: {length} limit: {limit}")
     """
     YOUR CODE HERE
     """
+    #i have a list of weights, and those weights have values that may or may not add up to the limit that is being passed. the idea is that i need to look through these in a hash table to find the values if they exist that add up and return their indices in the array/list.
 
+    #lets stick them into my little hash table, we will pass in the value as key as well as the index of it. ht is expecting a key and value -> def hash_table_insert(hash_table, key, value):
+    for i in range(0, length):
+        hash_table_insert(ht, weights[i], i)
+    
+    for i in range(0, length):
+        print(f"limit: {limit} weights[i]: {weights[i]}")
+        #lets begin a search for our values/keys
+        target = limit - weights[i]
+        # def hash_table_retrieve(hash_table, key):
+        answer = hash_table_retrieve(ht, target)
+        #if we find our answer and it exists in our ht, return it
+        if answer:
+            print(f"answer: {answer}, i: {i}")
+            #tuple()
+            return (answer, i)
+
+    # print(f"ht: {ht}")
     return None
 
+        # weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40] len: 9 lim: 7
+
+        # self.assertTrue(answer_4[0] == 6)
+        # self.assertTrue(answer_4[1] == 2)
 
 def print_answer(answer):
     if answer is not None:
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,8 +11,14 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+# where the source string represents the starting airport and the destination string represents the next airport along our trip. The ticket for your first flight has a destination with a source of NONE, and the ticket for your final flight has a source with a destination of NONE.
+
+# The crux of this problem requires us to 'link' tickets together to reconstruct the entire trip. For example, if we have a ticket ('SJC', 'BOS') that has us flying from San Jose to Boston, then there exists another ticket where Boston is the starting location, ('BOS', 'JFK').
+
+# We can hash each ticket such that the starting location is the key and the destination is the value. Then, when constructing the entire route, the ith location in the route can be found by checking the hash table for the i-1th location.
 
 def reconstruct_trip(tickets, length):
+    print(f"tickets: {tickets} length: {length}")
     hashtable = HashTable(length)
     route = [None] * length
 
